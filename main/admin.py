e1b78780082ef755d725fbe2fed85431f5c5e4be
Setsumi <butkovvlad2001@gmail.com>
2025-11-15 15:47:25 +0300

    Добавлены функции: Создание новостей в админке, создание тредов(постов) в админке

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import News, Thread
 
 # Register your models here.
+admin.site.register(News)
+admin.site.register(Thread)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    count_views = models.IntegerField(default=0, blank=True)
+    category = models.CharField(max_length=50)
+    # image = models.ImageField(upload_to="news_images/", blank=True, null=True)
+    image = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
+
+class Thread(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=50)
+    count_messages = models.IntegerField(default=0, blank=True)
+    count_views = models.IntegerField(default=0, blank=True)
+    last_message_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Тема форума"
+        verbose_name_plural = "Темы форума"
