b540cd44a9199920e56688c5d4e63fdfe6fa18e6
Setsumi <butkovvlad2001@gmail.com>
2025-11-21 15:51:21 +0300

    Добавлены функции: Отрисовка новостей и форумов

--- main/views.py
+++ main/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .forms import UserRegisterForm, UserLoginForm
+from .models import News, Thread
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/base.html')
+    news = News.objects.all().order_by('-created_at')[:3]
+    threads = Thread.objects.all().order_by('-created_at')[:6]
+
+    context = {
+        'news': news,
+        'threads': threads,
+    }
+    return render(request, 'main/base.html', context)
